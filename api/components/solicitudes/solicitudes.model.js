50bb91869cb01bf802031de2aff2d71ef3156e6e
Gmoranav <41559453+Gmoranav@users.noreply.github.com>
2018-07-23 17:55:04 -0600

    Create solicitudes.model.js

---
+++ api/components/solicitudes/solicitudes.model.js
@@ -0,0 +1,23 @@
+'use strict';
+let mongoose = require('mongoose'); /*Conexion con el server de MongoDB*/
+
+/*Esquema para los usuarios*/
+let solicitudSchema = new mongoose.Schema({
+    /**Apartir de aqui se crean las propiedades que se le solicita al usuario*/
+    /**Las propiedades para crear usuarios con rol */
+
+    primer_nombre : {type : String, required : true},
+    segundo_nombre : {type : String},
+    primer_apellido : {type : String, required : true},
+    segundo_apellido : {type : String, required : true},
+    curso : {type : String},
+    periodo : {type : String},
+    grupo : {type : String},
+    cantidad_alumnos : {type : Number, required : true},
+    horario : {type : String, required : true}
+    /*crear uno por input*/
+    
+}); 
+
+/*Test = el nombre de la carpeta que estamos trabajando en mayuscula y singular*/ 
+module.exports = mongoose.model('Solicitud', solicitudSchema);
