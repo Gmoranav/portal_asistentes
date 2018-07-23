eba54fdcc5796f8fbb142124d5bcfba5413f5beb
JoeAlvarado <41559472+JoeAlvarado@users.noreply.github.com>
2018-07-22 21:29:12 -0600

    Create service.registrarCarrera.js

---
+++ public/js/service.registrarCarrera.js
@@ -0,0 +1,48 @@
+'use strict';
+/**
+ * let lista_carrera =[];
+ */
+
+function registrar_carrera(psCodigoCarrera, psNombreCarrera, pnCreditosTotales, psFechaCreacion, sltSede){
+
+    let respuesta = '';
+    let peticion = $.ajax({
+
+        url : 'http://localhost:4000/api/registrar_carrera',
+        type : 'post',
+        contentType : 'application/x-www-form-urlencoded; charset=utf-8',
+        dataType : 'json',
+        async : false,
+        data:{
+
+            codigo_carrera : psCodigoCarrera,
+            nombre_carrera : psNombreCarrera,
+            creditos_totales : pnCreditosTotales,
+            fecha_creacion : psFechaCreacion,
+            slt_sede : sltSede
+
+        }
+
+    });
+
+    peticion.done(function(response){
+        respuesta = response;
+        console.log('envio exitoso');
+       });
+     
+       peticion.fail(function(response){
+         respuesta = response;
+         console.log('envio no exitoso');
+       });
+ 
+       return respuesta;
+
+
+};
+
+/***
+ * let nuevaCarrera = [];
+    nuevaCarrera.push(psCodigoCarrera, psNombreCarrera, pnCreditosTotales, psFechaCreacion, sltSede);
+
+    lista_carrera.push(nuevaCarrera);
+ */
