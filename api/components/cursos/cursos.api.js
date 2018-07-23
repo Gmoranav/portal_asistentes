23b5ed930459d0c52d439e8a7fd003b3a2ed9ac3
JoeAlvarado <41559472+JoeAlvarado@users.noreply.github.com>
2018-07-22 21:16:19 -0600

    cursos.api.js

---
+++ api/components/cursos/cursos.api.js
@@ -0,0 +1,36 @@
+'use strict';
+const cursoModel = require('./cursos.model');
+
+module.exports.registrar = function(req, res){
+
+    let nuevoCurso = new cursoModel({
+        nombre_curso : req.body.nombre_curso,
+        codigo_curso : req.body.codigo_curso,
+        slt_sede : req.body.slt_sede,
+        cantidad_creditos : req.body.cantidad_creditos,
+        slt_carreraCurso : req.body.slt_carreraCurso
+
+
+    });
+
+    nuevoCurso.save(function(error){
+        if(error){
+
+            res.json({
+                success : false,
+                msj : 'El curso no pudo ser registrado :' + error
+            });
+
+        }else{
+
+            res.json({
+                success : true,
+                msj : 'El curso se registró con éxito!'
+            });
+        }
+
+
+    });
+
+
+};
