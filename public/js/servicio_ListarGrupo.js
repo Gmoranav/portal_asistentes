e2436ee3c7855a5a09456d4fbe2efb0743d2e5e5
freddylobo <41560118+freddylobo@users.noreply.github.com>
2018-07-24 22:56:43 -0600

    Add files via upload

---
+++ public/js/servicio_ListarGrupo.js
@@ -0,0 +1,29 @@
+function obtenerListaGrupos(){
+
+    //cambiar Examples por lo que se vaya a listar. Debe estar en plural
+    let listaGrupos = []; //THIS
+
+    
+    let peticion = $.ajax({
+        //*cambiar examples en el url por lo que se vaya a registrar, debe estar en plural
+        url : 'http://localhost:4000/api/listar_grupos',
+        type : 'get',
+        contentType : 'application/x-www-form-urlencoded; charset=utf-8',
+        dataType : 'json',
+        async : false,
+        data:{
+            
+        }
+      });
+    
+      peticion.done(function(response){
+        listaGrupos = response;
+      });
+    
+      peticion.fail(function(response){
+        //listaGrupos = console.log('Importación fallida');
+      });
+
+      return listaGrupos;
+    //cambiar Examples por lo que se vaya a listar. Debe estar en plural
+};
