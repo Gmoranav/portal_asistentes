8a10669c1f0fde5b3656e1bb682d64d884ec4a85
freddylobo <41560118+freddylobo@users.noreply.github.com>
2018-07-24 22:55:18 -0600

    Add files via upload

---
+++ public/js/controlador_ListarLaboratorio.js
@@ -0,0 +1,35 @@
+'use strict';
+
+window.addEventListener('load', imprimirListaLaboratorios);
+
+function imprimirListaLaboratorios(){
+    
+    let listaLaboratorios = obtenerListaLaboratorios();
+    let tbody = document.querySelector('#tblLaboratorio');//Cambiar Examples por lo que se vaya a listar, usar nombre en plural
+                                                            //(ejemplo: cursos, sedes...)
+    /*if(!pFiltro){
+        pFiltro = '';
+    }*/
+    //tbody.innerHTML = '';
+
+
+    //Cambiar Examples por lo que se vaya a listar, usar nombre en plural (ejemplo: cursos, sedes...)
+    for(let i = 0; i < listaLaboratorios.length; i++){
+
+        /*los nombres que están entre corchetes y comillas simples
+        deben ser los mismos que están en la función registrarExamples del archivo servicio.
+        Están en la sección data{}.  NO los que vienen por parámetro sino lo que se declaran en
+        la función.  Se deben colocar en el mismo orden*/
+        let fila = tbody.insertRow();
+                    
+        let cNombreLaboratorio = fila.insertCell();
+        let cCantEspacios = fila.insertCell();
+        
+        
+        cNombreLaboratorio.innerHTML = listaLaboratorios[i]['nombre_laboratorio'];
+        cCantEspacios.innerHTML = listaLaboratorios[i]['cantidad_espacios'];
+        
+        
+    }
+
+};
