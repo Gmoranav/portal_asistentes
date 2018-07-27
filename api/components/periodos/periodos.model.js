ff4dd4380c7132f6d15e2a61c4ed2d8da2adf26b
daguilar16 <41559865+daguilar16@users.noreply.github.com>
2018-07-26 20:21:30 -0600

    Update periodos.model.js

--- api/components/periodos/periodos.model.js
+++ api/components/periodos/periodos.model.js
@@ -1,11 +1,11 @@
 'use strict';
 let mongoose = require('mongoose');
 
-let periodoSchema = new mongoose.Schema({
+let RegistrarPeriodoSchema = new mongoose.Schema({
     nombre_periodo : {type : String, required : true},
     estado_periodo : {type : String, required : true},
 });
 
 //Periodo debe ir en singular y en mayuscula
 //El nombre de la coleccion debe coincidir con el nombre en la base de datos
-module.exports = mongoose.model('Periodo' , periodoSchema);
+module.exports = mongoose.model('Periodo' , RegistrarPeriodoSchema);
