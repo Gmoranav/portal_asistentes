4471b2986aa8b67cf3a215a9ec2babbfeca95e7b
daguilar16 <41559865+daguilar16@users.noreply.github.com>
2018-07-27 23:04:50 -0600

    Update controlador_ListarPeriodo.js

--- public/js/controlador_ListarPeriodo.js
+++ public/js/controlador_ListarPeriodo.js
@@ -9,37 +9,25 @@ Responsabilidades del controlador
 
 'use strict';
 
-//const botonRegistrar = document.querySelector('#btnRegistrar');
-//botonRegistrar.addEventListener('click', listarUsuarios);
-
 window.addEventListener('load', listarPeriodos);
 
 function imprimirListaPeridodos(plistaPeriodos){
-    
     
     let tbody = document.querySelector('#tblListarPeriodos');
    
     tbody.innerHTML = '';
 
-
-    
     for(let i = 0; i < plistaPeriodos.length; i++){
 
- 
             let fila = tbody.insertRow();
-                        
-            
             let cnombre_periodo = fila.insertCell();
             let cestado_periodo = fila.insertCell();
-        
-        
+                
             cnombre_periodo.innerHTML = plistaPeriodos[i]['nombre_periodo'];
             cestado_periodo.innerHTML = plistaPeriodos[i]['estado_periodo'];
-            console.log(cestado_periodo);
-    
+            console.log(cestado_periodo);    
     }
 };
-
 
 function listarPeriodos(){
 
