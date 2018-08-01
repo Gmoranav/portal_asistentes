2508f338f4faf0e236ecdc94c2b97334423de379
freddylobo <41560118+freddylobo@users.noreply.github.com>
2018-08-01 00:48:48 -0600

    Update grupos.model.js

--- api/components/grupos/grupos.model.js
+++ api/components/grupos/grupos.model.js
@@ -10,9 +10,15 @@ let grupoSchema = new mongoose.Schema({
     /**Las propiedades para crear usuarios con rol */
 
     /**El example seria el input al que se le solicita al usuario */
+    sedeGrupo : {type : String, required : true},
+    carreraGrupo : {type : String, required : true},
+    cursoGrupo : {type : String, required : true},
+    periodoGrupo : {type : String, required : true},
     nombre : {type : String, required : true, unique : true},
     laboratorio : {type : String, required : true},
     profesor : {type : String, required : true},
+    profesor2 : {type : String, required : false},
+    profesor3 : {type : String, required : false},
     cantidad_de_estudiantes : {type : String, required : true},
     horario : {type : String, required : true}
 
