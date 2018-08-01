e0760a5f85cb12bb1854e2f9f2eb96d9ab6865a9
freddylobo <41560118+freddylobo@users.noreply.github.com>
2018-08-01 00:40:39 -0600

    Update servicio_RegistrarGrupo.js

--- public/js/servicio_RegistrarGrupo.js
+++ public/js/servicio_RegistrarGrupo.js
@@ -9,7 +9,8 @@ Responsabilidades del servicio
 
 //cambiar el nombre Example por lo que se esté registrando, debe estar en singular
 //cambiar los parametros manteniendo una s al principio cuando es texto y n cuando es numero
-function registrarGrupo(psNombre, psLaboratorio, psProfesor, psCantidadEstu, psHorario){
+function registrarGrupo(psSede, psCarrera, psCurso, psPeriodo, psNombre, psLaboratorio, psProfesor, 
+psProfesor2, psProfesor3, psCantidadEstu, psHorario){
     let respuesta = '';
     let peticion = $.ajax({
 
@@ -24,9 +25,15 @@ function registrarGrupo(psNombre, psLaboratorio, psProfesor, psCantidadEstu, psH
             //cambiar los nombres por lo que se requiera
             //las variables son las que hay que usan en el archivo controlador
             //en la función imprimirListaExamples
+            sedeGrupo : psSede,
+            carreraGrupo : psCarrera,
+            cursoGrupo : psCurso,
+            periodoGrupo : psPeriodo,
             nombre : psNombre,
             laboratorio : psLaboratorio,
             profesor : psProfesor,
+            profesor2 : psProfesor2,
+            profesor3 : psProfesor3,
             cantidad_de_estudiantes : psCantidadEstu,
             horario : psHorario
         }
@@ -44,34 +51,3 @@ function registrarGrupo(psNombre, psLaboratorio, psProfesor, psCantidadEstu, psH
 
     return respuesta;
 }
-
-//cambiar Examples por lo que se vaya a listar. Debe estar en plural
-/*function obtenerListaGrupos(){
-
-    //cambiar Examples por lo que se vaya a listar. Debe estar en plural
-    let listaGrupos = []; //THIS
-
-    
-    let peticion = $.ajax({
-        //*cambiar examples en el url por lo que se vaya a registrar, debe estar en plural
-        url : 'http://localhost:4000/api/listar_grupos',
-        type : 'get',
-        contentType : 'application/x-www-form-urlencoded; charset=utf-8',
-        dataType : 'json',
-        async : false,
-        data:{
-            
-        }
-      });
-    
-      peticion.done(function(response){
-        listaGrupos = response;
-      });
-    
-      peticion.fail(function(response){
-        //listaGrupos = console.log('Importación fallida');
-      });
-
-      return listaGrupos;
-    //cambiar Examples por lo que se vaya a listar. Debe estar en plural
-};*/
