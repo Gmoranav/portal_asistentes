b5fa09bc3384ad78f9a6c4f13d0767d30ec8d07f
freddylobo <41560118+freddylobo@users.noreply.github.com>
2018-08-23 15:30:10 -0600

    update servicio laboratorio
    update con funcionaes para eliminar, buscar por id,etc.

--- public/js/servicio_ListarLaboratorio.js
+++ public/js/servicio_ListarLaboratorio.js
@@ -1,4 +1,4 @@
-function obtenerListaLaboratorios(){
+function obtener_laboratorio_por_id (p_id) {
 
     //cambiar Examples por lo que se vaya a listar. Debe estar en plural
     let listaLaboratorios = []; //THIS
@@ -6,8 +6,8 @@ function obtenerListaLaboratorios(){
     
     let peticion = $.ajax({
         //*cambiar examples en el url por lo que se vaya a registrar, debe estar en plural
-        url : 'http://localhost:4000/api/listar_laboratorio',
-        type : 'get',
+        url : 'http://localhost:4000/api/buscar_laboratorio_por_id',
+        type : 'post',
         contentType : 'application/x-www-form-urlencoded; charset=utf-8',
         dataType : 'json',
         async : false,
@@ -26,4 +26,29 @@ function obtenerListaLaboratorios(){
 
       return listaLaboratorios;
     //cambiar Examples por lo que se vaya a listar. Debe estar en plural
+
+}
+
+function eliminar_laboratorio(p_id){
+  let respuesta = '';
+  let peticion = $.ajax({
+      url : 'http://localhost:4000/api/eliminar_laboratorio',
+      type : 'post',
+      contentType : 'application/x-www-form-urlencoded; charset=utf-8',
+      dataType : 'json',
+      async : false,
+      data:{
+          _id: p_id
+      }
+    });
+  
+    peticion.done(function(response){
+     respuesta = response;
+    });
+  
+    peticion.fail(function(response){
+     
+    });
+
+    return respuesta;
 };
