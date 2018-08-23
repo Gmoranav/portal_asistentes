131b68f51a01991183e607b3514e79025b8070ef
freddylobo <41560118+freddylobo@users.noreply.github.com>
2018-08-23 16:07:59 -0600

    update servicio listar laboratorio

--- public/js/servicio_ListarLaboratorio.js
+++ public/js/servicio_ListarLaboratorio.js
@@ -1,3 +1,34 @@
+function obtenerListaLaboratorios(){
+
+  //cambiar Examples por lo que se vaya a listar. Debe estar en plural
+  let listaLaboratorios = []; //THIS
+
+  
+  let peticion = $.ajax({
+      //*cambiar examples en el url por lo que se vaya a registrar, debe estar en plural
+      url : 'http://localhost:4000/api/listar_laboratorio',
+      type : 'get',
+      contentType : 'application/x-www-form-urlencoded; charset=utf-8',
+      dataType : 'json',
+      async : false,
+      data:{
+          
+      }
+    });
+  
+    peticion.done(function(response){
+      listaLaboratorios = response;
+    });
+  
+    peticion.fail(function(response){
+      //listaLaboratorios = console.log('Importación fallida');
+    });
+
+    return listaLaboratorios;
+  //cambiar Examples por lo que se vaya a listar. Debe estar en plural
+};
+
+
 function obtener_laboratorio_por_id (p_id) {
 
     //cambiar Examples por lo que se vaya a listar. Debe estar en plural
