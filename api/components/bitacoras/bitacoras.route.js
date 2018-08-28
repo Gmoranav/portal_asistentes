77e7c9346f836068afab3430c0d5c3957ba53d34
yanorymr <41559492+yanorymr@users.noreply.github.com>
2018-08-28 02:46:29 -0600

    Update bitacoras.route.js

--- api/components/bitacoras/bitacoras.route.js
+++ api/components/bitacoras/bitacoras.route.js
@@ -18,14 +18,30 @@ router.route('/listar_bitacoras')
     bitacorasApi.listar_bitacoras(req, res);
 });
 
-router.route('/buscar_bitacoras_id')
+router.route('/buscar_bitacora_por_id')
     .post(function(req, res){
-    bitacorasApi.buscar_bitacoras_por_id(req, res);
+    bitacorasApi.buscar_bitacora_por_id(req, res);
 });
 
 router.route('/modificar_bitacora')
     .post(function(req, res){
     bitacorasApi.modificar_bitacora(req, res);
 });
+
+router.route('/aprobar_bitacora')
+    .post(function(req, res){
+    bitacorasApi.aprobar_bitacora(req, res);
+});
+
+router.route('/desactivar_bitacora')
+    .post(function(req, res){
+    bitacorasApi.desactivar_bitacora(req, res);
+});
+
+router.route('/rechazar_bitacora')
+    .post(function(req, res){
+    bitacorasApi.rechazar_bitacora(req, res);
+});
+
 
 module.exports = router;
